8a423b2e4094ac84320d9d52214aa5c3639e4a72
Nguyen Hoang <hoangnv@outlook.com>
2021-06-05 18:57:40 +0700

    feat: add home page

---
+++ src/components/VideoCard/VideoCard.tsx
@@ -0,0 +1,32 @@
+import { Video } from '../../graphql/graphql';
+import './VideoCard.css';
+
+export interface VideoCardProps {
+  video: Video;
+}
+
+export const VideoCard = ({ video }: VideoCardProps) => {
+  return (
+    <div className="flex gap-8 w-full">
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${video.youtubeId}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen></iframe>
+      </div>
+      <div className="w-full">
+        <h3 className="text-red-500 font-bold">{video.title}</h3>
+        <div className="text-gray-500">
+          Shared by:{' '}
+          <span className="font-medium text-black">{video.user.account}</span>
+        </div>
+        <span className="text-gray-500">Description:</span>
+        <p className="video__description font-medium">{video.description}</p>
+      </div>
+    </div>
+  );
+};
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,56 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { VideoCard } from '../../components/VideoCard/VideoCard';
+import { useVideosLazyQuery, Video } from '../../graphql/graphql';
+
 export const Home = () => {
-  return <div>Home</div>;
+  const [videos, setVideos] = useState<Video[]>([]);
+  const [hasMore, setHashMore] = useState(true);
+  const [getVideos, { loading, data, error }] = useVideosLazyQuery({
+    onCompleted: (data) => {
+      if (data.videos.length) {
+        setVideos([...videos, ...data.videos]);
+      }
+      if (!data.videos.length) {
+        setHashMore(false);
+      }
+    }
+  });
+
+  const loadMore = useCallback(() => {
+    getVideos({
+      variables: {
+        offset: videos.length
+      }
+    });
+  }, [getVideos, videos.length]);
+
+  useEffect(() => {
+    getVideos();
+  }, [getVideos]);
+
+  if (error) {
+    return <div className="container mx-auto">Oops! Please try again...</div>;
+  }
+
+  return (
+    <div className="container mx-auto p-5">
+      {videos.map((video) => (
+        <div key={video.id} className="xl:w-3/4 w-full mx-auto mb-8">
+          <VideoCard video={video}></VideoCard>
+        </div>
+      ))}
+      {hasMore && (
+        <div className="w-full text-center">
+          {loading ? (
+            <button className="btn">Loading...</button>
+          ) : (
+            <button className="btn" onClick={loadMore}>
+              Load more
+            </button>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
